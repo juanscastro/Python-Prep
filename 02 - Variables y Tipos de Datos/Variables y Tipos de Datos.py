33da94ee667a8fbf1dcf64e5419ee8006808e3c2
Your Name <juansimoncastro@gmail.com>
2023-01-25 10:22:33 -0300

    TEST

---
+++ 02 - Variables y Tipos de Datos/Variables y Tipos de Datos.py
@@ -0,0 +1,114 @@
+# 01) Crear una variable que contenga un elemento del conjunto de números enteros y luego imprimir por pantalla
+
+x = 5
+print(x)
+
+# 02) Imprimir el tipo de dato de la constante 8.5
+
+type(8.5)
+
+# 03) Imprimir el tipo de dato de la variable creada en el punto 1
+
+type(x)
+
+# 04) Crear una variable que contenga tu nombre
+
+name = 'Juan'
+
+# 05) Crear una variable que contenga un número complejo
+
+z = 1 + 1j
+
+# 06) Mostrar el tipo de dato de la variable crada en el punto 5
+
+type(z)
+
+# 07) Crear una variable que contenga el valor del número Pi redondeado a 4 decimales
+
+pi = 3.1415
+
+# 08) Crear una variable que contenga el valor 'True' y otra que contenga el valor True. ¿Se trata de lo mismo?
+
+k = 'True'
+m = True
+
+# 09) Imprimir el tipo de dato correspondientes a las variables creadas en el punto 9
+
+print(type(k))
+
+# 10) Asignar a una variable, la suma de un número entero y otro decimal
+
+q = 4 + 10.5
+
+# 11) Realizar una operación de suma de números complejos
+
+10j + 25j
+
+# 12) Realizar una operación de suma de un número real y otro complejo
+
+10 + 25j
+
+# 13) Realizar una operación de multiplicación
+
+8 * 5
+
+# 14) Mostrar el resultado de elevar 2 a la octava potencia
+
+print(2**8)
+
+# 15) Obtener el cociente de la división de 27 entre 4 en una variable y luego mostrarla
+
+g = 27 / 4
+print(g)
+
+# 16) De la división anterior solamente mostrar la parte entera
+
+g = 27 // 4
+print(g)
+
+# 17) De la división de 27 entre 4 mostrar solamente el resto
+
+print(27 % 4)
+
+# 18) Utilizando como operandos el número 4 y los resultados obtenidos en los puntos 16 y 17. Obtener 27 como resultado
+
+g = 27 // 4
+e = 27 % 4
+print((g * 4)+e)
+
+# 19) Utilizar el operador "+" en una operación donde intervengan solo variables alfanuméricas
+
+var1 = 'Mi Casa'
+var2 = 'Bienvenidos a '
+print(var2 + var1)
+
+# 20) Evaluar si "2" es igual a 2. ¿Por qué ocurre eso?
+
+"2" == 2
+
+# Utilizar las funciones de cambio de tipo de dato, para que la validación del punto 20 resulte verdadera
+
+int("2") == 2
+
+# 22) ¿Por qué arroja error el siguiente cambio de tipo de datos? a=float('3,8')
+
+a = float('3.8')
+
+# 23) Crear una variable con el valor 3, y utilizar el operador '-=' para modificar su contenido
+
+var3 = 3
+var3 -= 1
+print(var3)
+
+# 24) Realizar la operacion 1 << 2 ¿Por qué da ese resultado? ¿Qué es el sistema de numeración binario?
+
+1 << 2
+
+# 25) Realizar la operación 2 + '2' ¿Por qué no está permitido? ¿Si los dos operandos serían del mismo tipo, siempre arrojaría el mismo resultado?
+
+2 + '2'
+2 + int('2')
+
+# 26) Realizar una operación válida entre valores de tipo entero y string
+
+'ja ' * 3
